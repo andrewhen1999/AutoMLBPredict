6dfbc15f2dac2257a31d29dcc104b42965944f14
andrewhen1999 <82618774+andrewhen1999@users.noreply.github.com>
2026-07-28 18:06:06 -0400

    Update predictions_history.py

--- MLBPredict/files/predictions_history.py
+++ MLBPredict/files/predictions_history.py
@@ -8,4 +8,4 @@ df2 = pd.read_csv(r"C:\Users\andre\OneDrive\Desktop\MLBAttempt\AutoMLBPredict\ML
 combined_df = pd.concat([df1, df2], ignore_index=True)
 
 # Save back to the destination file
-combined_df.to_csv(r"C:\Users\andre\OneDrive\Desktop\MLBAttempt\AutoMLBPredict\MLBPredict\output\predictions_history.csv", index=False)
+combined_df.to_csv(r"MLBPredict/output/predictions_history.csv" , index=False)
